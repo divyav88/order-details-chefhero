82202dab0a69021756c6c640e4be8b63ed2694c7
divyav88 <divya.viswanath02@gmail.com>
2020-10-26 22:29:31 -0400

    Fixing deployment issues

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ dotenv.config({
 
 const app = express();
 
-if (process.env.NODE_ENV === "production") {
+if (process.env.SERVER_ENV === "production") {
   app.use(express.static("client/build"));
   app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
