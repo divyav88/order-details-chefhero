d2110af4d3392ae9d30a4c68c0fef42c85853f7d
divyav88 <divya.viswanath02@gmail.com>
2020-10-26 21:58:50 -0400

    Fixing deployment issues

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,10 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          ChefHero Order Details
-        </a>
+        <p>ChefHero Order Details</p>
       </header>
     </div>
   );
